6bc1b38493ee2ee45e079c2c58e3baf429c565a9
JacoboGarcesOP <jacobo.garces@pragma.com.co>
2025-10-07 11:01:28 -0500

    feat: Add database schema for branches and products. Created tables for branches and products with foreign key relationships to franchise and branch respectively, along with necessary indexes for efficient querying.

--- applications/app-service/src/main/resources/schema.sql
+++ applications/app-service/src/main/resources/schema.sql
@@ -8,3 +8,30 @@ CREATE TABLE IF NOT EXISTS nequi_schema.franchise (
 CREATE INDEX IF NOT EXISTS idx_franchise_name ON nequi_schema.franchise(name);
 
 
+-- Branches table: one-to-many with franchise
+CREATE TABLE IF NOT EXISTS nequi_schema.branch (
+    branch_id BIGSERIAL PRIMARY KEY,
+    franchise_id BIGINT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    CONSTRAINT fk_branch_franchise
+        FOREIGN KEY (franchise_id)
+        REFERENCES nequi_schema.franchise(franchise_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_branch_franchise_id ON nequi_schema.branch(franchise_id);
+
+-- Products table: one-to-many with branch
+CREATE TABLE IF NOT EXISTS nequi_schema.product (
+    product_id BIGSERIAL PRIMARY KEY,
+    branch_id BIGINT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    stock INT NOT NULL,
+    CONSTRAINT fk_product_branch
+        FOREIGN KEY (branch_id)
+        REFERENCES nequi_schema.branch(branch_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_product_branch_id ON nequi_schema.product(branch_id);
+
